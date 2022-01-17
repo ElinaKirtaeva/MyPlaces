763f11234a784f6b0ce4814fcb6af618b753eba6
Elina Kirtaeva <elin-ru1@yandex.ru>
2022-01-17 18:58:52 +0300

    maps

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -7,23 +7,34 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
 
     var place = Place()
     let annotationIdentifier = "annotationIdentifier"
+    let locationManager = CLLocationManager()
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         setupPlacemark()
+        checkLocationServices()
 
     }
 
     @IBAction func closeVC() {
         dismiss(animated: true)
     }
+    
+    @IBAction func centerViewInUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
     
     func setupPlacemark() {
         guard let location = place.location else {return}
@@ -45,6 +56,47 @@ class MapViewController: UIViewController {
             self.mapView.selectAnnotation(annotation, animated: true)
                     
         }
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAutorization()
+        } else {
+            
+        }
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func checkLocationAutorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways:
+            mapView.showsUserLocation = true
+            break
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            break
+        case .denied:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            break
+        @unknown default:
+            print("New case is available")
+        }
+    }
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
 
@@ -68,4 +120,10 @@ extension MapViewController: MKMapViewDelegate {
         return annotationView
     }
     
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAutorization()
+    }
 }
